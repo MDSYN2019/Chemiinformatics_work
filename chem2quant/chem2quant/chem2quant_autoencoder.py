f29da25da287f2b9564809f415d2dfc97813c762
Sang Young Noh <gnasnoh123@googlemail.com>
2019-01-12 19:15:05 +0000

    Working autoencoder

--- chem2quant/chem2quant/chem2quant_autoencoder.py
+++ chem2quant/chem2quant/chem2quant_autoencoder.py
@@ -5,6 +5,8 @@ import pandas as pd
 import os
 import sys
 
+from functools import partial
+
 import tensorflow as tf
 from tensorflow.contrib.layers import fully_connected
 
@@ -97,50 +99,99 @@ embed = max([len(smile) for smile in data.smiles]) + 5
 
 X_train, Y_train = vectorize(smiles_train.values)
 X_test, Y_test = vectorize(smiles_test.values)
+X_train, Y_train = vectorize(smiles_train.values)
 
-class_1, class2 = np.unique(Y_train, axis = 0)
+X_train = [a.flatten() for a in X_train]
+X_test = [a.flatten() for a in X_test]
+Y_train = [a.flatten() for a in Y_train]
+Y_test = [a.flatten() for a in Y_test]
 
-smiles_len = len(X_train[0])
+#class_1, class2 = np.unique(Y_train, axis = 0)
+#smiles_len = len(X_train[0])
 
-# ---------------------------#
-# Neural network below here  #
-# ---------------------------#
+# -------------------------------#
+#  Neural network - autoencoder  #
+# -------------------------------#
 
-n_steps, n_inputs = np.shape(X_train[0])
-n_epochs = 100 # How many times do we do a back and front propagation?
-batch_size = 200 
-n_neurons = 200
-n_outputs = 2
+n_inputs = 27 * 22
+n_hidden1 = 300
+n_hidden2 = 150
+n_hidden3 = n_hidden1
+n_outputs = n_inputs
+
 learning_rate = 0.001
+l2_reg = 0.0001
 
+n_hidden1 = 300
+n_hidden2 = 150 # codings
 
-X = tf.placeholder(tf.float32, [None, n_steps, n_inputs])
-y = tf.placeholder(tf.int32, [None])
+X = tf.placeholder(tf.float32, [None, n_inputs])
+he_init = tf.contrib.layers.variance_scaling_initializer() # He initialization
+l2_regularizer = tf.contrib.layers.l2_regularizer(l2_reg)
 
-basic_cell = tf.contrib.rnn.BasicRNNCell(num_units=n_neurons)
-outputs, states = tf.nn.dynamic_rnn(basic_cell, X, dtype=tf.float32)
-logits = tf.layers.dense(states, n_outputs)
-xentropy = tf.nn.sparse_softmax_cross_entropy_with_logits(labels=y, logits=logits)
+my_dense_layer = partial(tf.layers.dense,
+                         activation=tf.nn.elu,
+                         kernel_initializer=he_init,
+                         kernel_regularizer=l2_regularizer)
 
-loss = tf.reduce_mean(xentropy)
-optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate)
+hidden1 = my_dense_layer(X, n_hidden1)
+hidden2 = my_dense_layer(hidden1, n_hidden2)
+hidden3 = my_dense_layer(hidden2, n_hidden3)
+outputs = my_dense_layer(hidden3, n_outputs, activation=None)
+
+reconstruction_loss = tf.reduce_mean(tf.square(outputs - X))
+
+reg_losses = tf.get_collection(tf.GraphKeys.REGULARIZATION_LOSSES)
+loss = tf.add_n([reconstruction_loss] + reg_losses)
+
+optimizer = tf.train.AdamOptimizer(learning_rate)
 training_op = optimizer.minimize(loss)
-correct = tf.nn.in_top_k(logits, y, 1)
-accuracy = tf.reduce_mean(tf.cast(correct, tf.float32))
-
-# Multiple layer RNN
 
 init = tf.global_variables_initializer()
 
-# This should work ..
+n_epochs = 5
+batch_size = 150
 
 with tf.Session() as sess:
     init.run()
     for epoch in range(n_epochs):
-        for iteration in range(len(X_train) // batch_size):
+        n_batches = len(X_train) // batch_size
+        for iteration in range(n_batches):
+            print("\r{}%".format(100 * iteration // n_batches), end="") # not shown in the book
+            sys.stdout.flush()                                          # not shown
             X_batch, y_batch = next_batch(batch_size, X_train, Y_train)
-            X_batch = X_batch.reshape((-1, n_steps, n_inputs))
-            sess.run(training_op, feed_dict={X: X_batch, y: y_batch})
-            acc_train = accuracy.eval(feed_dict={X: X_batch, y: y_batch})
-            acc_test = accuracy.eval(feed_dict={X: X_test, y: y_test})
-    print(epoch, "Train accuracy:", acc_train, "Test accuracy:", acc_test)
+            sess.run(training_op, feed_dict={X: X_batch})
+        loss_train = reconstruction_loss.eval(feed_dict={X: X_batch})   # not shown
+        print("\r{}".format(epoch), "Train MSE:", loss_train)           # not shown
+        #saver.save(sess, "./my_model_all_layers.ckpt")                  # not shown
+
+
+#y = tf.placeholder(tf.int32, [None])
+
+#basic_cell = tf.contrib.rnn.BasicRNNCell(num_units=n_neurons)
+#outputs, states = tf.nn.dynamic_rnn(basic_cell, X, dtype=tf.float32)
+#logits = tf.layers.dense(states, n_outputs)
+#xentropy = tf.nn.sparse_softmax_cross_entropy_with_logits(labels=y, logits=logits)
+
+#loss = tf.reduce_mean(xentropy)
+#optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate)
+#training_op = optimizer.minimize(loss)
+#correct = tf.nn.in_top_k(logits, y, 1)
+#accuracy = tf.reduce_mean(tf.cast(correct, tf.float32))
+
+# Multiple layer RNN
+
+#init = tf.global_variables_initializer()
+
+# This should work ..
+
+#with tf.Session() as sess:
+#    init.run()
+#    for epoch in range(n_epochs):
+#        for iteration in range(len(X_train) // batch_size):
+#            X_batch, y_batch = next_batch(batch_size, X_train, Y_train)
+#            X_batch = X_batch.reshape((-1, n_steps, n_inputs))
+#            sess.run(training_op, feed_dict={X: X_batch, y: y_batch})
+#            acc_train = accuracy.eval(feed_dict={X: X_batch, y: y_batch})
+#            acc_test = accuracy.eval(feed_dict={X: X_test, y: y_test})
+#    print(epoch, "Train accuracy:", acc_train, "Test accuracy:", acc_test)
