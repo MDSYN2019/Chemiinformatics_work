53c8fc87b03dd058a028c04579d77e651220c54f
Sang Young Noh <gnatnoh123@googlemail.com>
2019-01-29 17:09:58 +0000

    new

--- Chemistry2quant/src/chemistry2quant/chem2quant_gen_psi4_input.py
+++ Chemistry2quant/src/chemistry2quant/chem2quant_gen_psi4_input.py
@@ -1,12 +1,13 @@
 import psi4
 import numpy as np
-from rdkit import Chem
+OBfrom rdkit import Chem
 from rdkit.Chem import AllChem
 from rdkit.Chem.Draw import IPythonConsole
 
-
 class chem2quant_psi4:    
     """
+    Generate coordinate file from smiles for calculation with 
+    psi4
     """
     def mol2psi4(mol):
         mol = Chem.AddHs(mol)
--- Chemistry2quant/src/chemistry2quant/chem2quant_lipophilicity.py
+++ Chemistry2quant/src/chemistry2quant/chem2quant_lipophilicity.py
@@ -1,20 +1,12 @@
-from __future__ import print_function
-from functools import reduce
-from multiprocessing import Pool, cpu_count
-
-
 """                                                                                                                                                  
-RDKit modules                                                                                                                                        
 
-MurckoScaffold - meaning -  
 
-rdFMCS - meaning - 
-
-Morgan Fingerprints - This family of fingerprints, better known as circular fingerprints, is built by applying the Morgan algorithm 
-                      to a set of user-supplied atom invariants.  
 
 """
 
+from __future__ import print_function
+from functools import reduce
+from multiprocessing import Pool, cpu_count
 
 import sys
 import numpy as np
@@ -37,33 +29,28 @@ from rdkit.Chem.Fingerprints import FingerprintMols # Fingerprinting
 from rdkit import DataStructs
 
 # For comparing the rdkit smiles
-
 # sklearn metric modules
 
 from sklearn.metrics import accuracy_score
 from sklearn.metrics import log_loss
 
-# ensemble modules
+# Ensemble modules
 
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.ensemble import ExtraTreesClassifier
 from sklearn.ensemble import GradientBoostingClassifier
 from sklearn.svm import SVC
 
-# sklearn metrics
+# Sklearn metrics
 
 from xgboost import XGBClassifier
 from sklearn.metrics import classification_report
 from sklearn.metrics import confusion_matrix
 
-# This is new class for blending
-from blending_classification import BlendingClassifier
-
 zipFileDir = zipfile.ZipFile('../zip/lipophilicity.zip')
 Pd_df = pd.read_csv(zipFileDir.open('Lipophilicity.csv'))
 
-# tensorflow
-
+# Tensorflow
 import tensorflow as tf
 from tensorflow.contrib.tensorboard.plugins import projector
 
@@ -98,6 +85,7 @@ class rdkit_lipophilicity:
             DataStructs.ConvertToNumpyArray(fp, arr)
             np_fps.append(arr)
         self.np_fps = np_fps
+
     def load_tensorflow(learning_rate, training_epochs, batch_size):
         num_inputs = np.shape(self.np_fps[0])[0]
         
@@ -107,6 +95,7 @@ class rdkit_lipophilicity:
         self.batch_size = batch_size # What is the batch size?
         self.learning_rate = learning_rate 
         self.training_epochs = training_epochs
+        
     def weights():
         self.weights = {
             'h1': tf.Variable(tf.random_normal([num_input, n_hidden_1])),
@@ -143,6 +132,7 @@ class rdkit_lipophilicity:
 """
 Train the models
 """
+
 with tf.Session() as sess:
     # Run the initializer
     sess.run(init)
@@ -164,69 +154,3 @@ with tf.Session() as sess:
     print("Testing Accuracy:", \
         sess.run(accuracy, feed_dict={X: mnist.test.images,
                                       Y: mnist.test.labels}))
-
-def load_lipo(featurizer='ECFP', split='index', reload=True, move_mean=True):
-  """Load Lipophilicity datasets."""
-  # Featurize Lipophilicity dataset
-  logger.info("About to featurize Lipophilicity dataset.")
-  logger.info("About to load Lipophilicity dataset.")
-  data_dir = deepchem.utils.get_data_dir()
-  if reload:
-    if move_mean:
-      dir_name = "lipo/" + featurizer + "/" + str(split)
-    else:
-      dir_name = "lipo/" + featurizer + "_mean_unmoved/" + str(split)
-    save_dir = os.path.join(data_dir, dir_name)
-
-  dataset_file = os.path.join(data_dir, "Lipophilicity.csv")
-  if not os.path.exists(dataset_file):
-    deepchem.utils.download_url(
-        'http://deepchem.io.s3-website-us-west-1.amazonaws.com/datasets/Lipophilicity.csv'
-    )
-
-  Lipo_tasks = ['exp']
-
-  if reload:
-    loaded, all_dataset, transformers = deepchem.utils.save.load_dataset_from_disk(
-        save_dir)
-    if loaded:
-      return Lipo_tasks, all_dataset, transformers
-
-  if featurizer == 'ECFP':
-    featurizer = deepchem.feat.CircularFingerprint(size=1024)
-  elif featurizer == 'GraphConv':
-    featurizer = deepchem.feat.ConvMolFeaturizer()
-  elif featurizer == 'Weave':
-    featurizer = deepchem.feat.WeaveFeaturizer()
-  elif featurizer == 'Raw':
-    featurizer = deepchem.feat.RawFeaturizer()
-
-  loader = deepchem.data.CSVLoader(
-      tasks=Lipo_tasks, smiles_field="smiles", featurizer=featurizer)
-  dataset = loader.featurize(dataset_file, shard_size=8192)
-
-  # Initialize transformers
-  transformers = [
-      deepchem.trans.NormalizationTransformer(
-          transform_y=True, dataset=dataset, move_mean=move_mean)
-  ]
-
-  logger.info("About to transform data")
-  for transformer in transformers:
-    dataset = transformer.transform(dataset)
-
-  if split == None:
-    return Lipo_tasks, (dataset, None, None), transformers
-
-  splitters = {
-      'index': deepchem.splits.IndexSplitter(),
-      'random': deepchem.splits.RandomSplitter(),
-      'scaffold': deepchem.splits.ScaffoldSplitter()
-  }
-  splitter = splitters[split]
-  train, valid, test = splitter.train_valid_test_split(dataset)
-
-  if reload:
-    deepchem.utils.save.save_dataset_to_disk(save_dir, train, valid, test,
-                                             transformers)
-return Lipo_tasks, (train, valid, test), transformers
