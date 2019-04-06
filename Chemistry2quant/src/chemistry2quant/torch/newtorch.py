3cc26ff8615cd99ec229d0980d61a949c0e65042
Sang <gnatnoh123@googlemail.com>
2019-04-06 18:41:10 +0100

    new

---
+++ Chemistry2quant/src/chemistry2quant/torch/newtorch.py
@@ -0,0 +1,11 @@
+from __future__ import division
+from __future__ import unicode_literals
+
+import numpy as np
+from rdkit import Chem
+import multiprocessing
+import logging
+import torch
+from torch_geometric.data import Data
+
+
