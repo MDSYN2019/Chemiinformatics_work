9fa9dc861273c30dcc785012b5b8d33eb3231a85
Sang <gnatnoh123@googlemail.com>
2019-09-09 16:39:27 +0100

    new

---
+++ Chemistry2quant/src/chemistry2quant/test/test.py
@@ -0,0 +1,12 @@
+import os
+import sys
+import pytest
+import subprocess
+sys.path.append(os.path.abspath(os.path.join('..', '.'))) # Reference to the src path                                                               
+
+import chem2quant_analysis
+import chem2quant_autoencoder
+
+def test_analysis():                    
+	pass
+
